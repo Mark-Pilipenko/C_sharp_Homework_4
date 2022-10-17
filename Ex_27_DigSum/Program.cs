9df91e31947c792bcba602abb48b31195200f079
Mark <kalmark@mail.ru>
2022-10-17 22:45:50 +0300

    Add Ex_27_DigSum

---
+++ Ex_27_DigSum/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр числа {A} равна {DigSum(A)}");
+
+int DigSum(int num)
+{
+    num = Math.Abs(num);
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
